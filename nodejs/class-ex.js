0a6408cd944687e7b707d11b1dd72a35a843325e
MINH-THONG <minhthong@gmail.com>
2018-10-17 12:08:54 +0300

    add node js example

---
+++ nodejs/class-ex.js
@@ -0,0 +1,14 @@
+class Car {
+    constructor() {
+        this.brand = '';
+        this.model = '';
+        this.year = 0;
+    }
+}
+
+var car1 = new Car();
+
+
+var car2 = new Car();
+
+console.log('I am from class-ex.js');
